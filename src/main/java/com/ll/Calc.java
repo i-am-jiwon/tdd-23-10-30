0526ba85250a35af2e7b8a02f930e9aa41c79922
i-am-jiwon <dlark1128@gmail.com>
2023-10-30 09:49:23 +0900

    Calc

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,11 @@
+package com.ll;
+
+public class Calc {
+    public static int run(String math) {
+
+//        String[] nums = math.split("\\*");
+//        nums[0] = nums[0]
+
+        return 1;
+    }
+}
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,6 +2,7 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(Calc.run("3 * 1") == 3);
     }
 }
